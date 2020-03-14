c687885604aaa4586f3d57bd4cfcc44ba2bf20f2
Prasanna-chandorkar <prasanna03chandorkar@gmail.com>
2020-03-14 14:07:55 +0530

    myproj to make form

---
+++ for_form_design/SeminarInvites/SeminarInvites/Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SeminarInvites.Models
+{
+    public class UserContext :DbContext
+    {
+        public UserContext():base("UserContext")
+        {
+
+        }
+        public DbSet<GuestResponse> Guest { get; set; }
+    }
+}
