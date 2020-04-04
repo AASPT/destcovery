1899d2b1e6af8eed5a7d40d6ae83715c7ec3aa8a
PatilKajal97 <patilkajal11297@gmail.com>
2020-04-04 12:29:50 +0530

    done from this end final commit of kajal

---
+++ 02-04-2020 12.00 PM/DestcoveryTest/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DestCovery;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using System.Text;
+using DestCovery.Controllers;
+
+namespace DestcoveryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void ContactUs()
+
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.ContactUs() as ViewResult;
+
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
