bf846e5529b4b968b685a595520b01162caaf6d5
Ajaykumar87 <ajaykumar.challa87@gmail.com>
2020-04-04 13:08:11 +0530

    Version 4 and my final edited one

---
+++ Version 4/DestCovery/Models/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DestCovery.Models
+{
+    public class ViewModel
+    {
+        public List<Package_Master> package_master_vm { get; set; }
+
+        public List<Package_Spots> package_spots_vm { get; set; }
+
+        public List<Package_Tour> package_tour_vm { get; set; }
+
+        public List<Package_Grid_Image> package_grid_vm { get; set; }
+
+        public List<Package_Review> package_review_vm { get; set; }
+
+        public Admin_Info admin_info_vm { get; set; }
+
+        public List<User_Info> user_info_vm { get; set; }
+
+        public List<Bookings> booking_vm { get; set; }
+        
+    }
+}
